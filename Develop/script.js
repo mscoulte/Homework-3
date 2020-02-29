4422b7730806ff28de4f2f736ccf1d6dc4107b51
mscoulte <mscoulte@gmail.com>
2020-02-28 19:18:12 -0500

    It's actually working!

--- Develop/script.js
+++ Develop/script.js
@@ -6,57 +6,50 @@ function generatePassword(){
   let lowercaseYes = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
   let numbersYes = ['0','1','2','3','4','5','6','7','8','9'];
   let scharsYes = ['!','@','#','$','%','^','&','*','=','+','~','/','?',':',';'];
-  let randomBank = [];
+  let randomArr = [];
 
-   alert("Please confirm your password criteria.");
- 
-  
-  
-  
-
-  while (true){
+  confirmDetails = alert("Please confirm your password criteria.");
    
-    let passwordLength = prompt("How many characters would you like your password to be? (8-128)");
+  while (true){
+  
+   passwordLength = prompt("How many characters would you like your password to be? (8-128)");
 
-   if (passwordLength < 8){
-     alert("Your password must be at least 8 characters!")
-     passwordLength = prompt("How many characters would you like your password to be? (8-128)");
-     continue
+   if (passwordLength < 8 || passwordLength > 128){
+     alert("Invalid password length! Must be between 8-128 characters.")
    }
-   else if (passwordLength > 128){
-     alert("Your password must be less than 128 characters!")
-     passwordLength = prompt("How many characters would you like your password to be? (8-128)");
-     continue
-   }
+   
    else {
     confirm("Your password will be " + passwordLength + " characters.") 
     break
    }
   }
-   while (true){
+   
   
-  let lCase = confirm("Would you like your password to include lowercase characters?");
+  while (true){
+  
+  lCase = confirm("Would you like your password to include lowercase characters?");
   if (lCase){
     alert ("Your password WILL contain lowercase characters. ")
   }
   else
   alert("Your password will NOT contain lowercase characters.");
   
-  let uCase = confirm("Would you like your password to include uppercase characters?");
+  uCase = confirm("Would you like your password to include uppercase characters?");
   if (uCase){
     alert ("Your password WILL contain uppercase characters. ")
   }
-  else
+  else{
   alert("Your password will NOT contain uppercase characters.");
+  }
 
-  let numeric = confirm("Would you like your password to include numeric characters?");
+  numeric = confirm("Would you like your password to include numeric characters?");
   if (numeric){
     alert ("Your password WILL contain numeric characters. ")
   }
   else
   alert("Your password will NOT contain numeric characters.");
   
-  let specchar = confirm("Would you like your password to include special characters?");
+  specchar = confirm("Would you like your password to include special characters?");
   if (specchar){
     alert ("Your password WILL contain special characters. ")
   }
@@ -64,51 +57,101 @@ function generatePassword(){
   alert("Your password will NOT contain special characters.")
   
   if (lCase === false && uCase === false && numeric === false && specchar === false){
-  alert("Please generate a new password.")
-  
+  alert("We require at least 1 character type. Please generate a new password.")
 }
-break
+else{
+  break
+  }
 }
 
 if (lCase){
-  randomBank.push(lowercaseYes)
+  randomArr.push(...lowercaseYes)
 }
 if (uCase){
-  randomBank.push(uppercaseYes)
+  randomArr.push(...uppercaseYes)
 }
 if (numeric){
-  randomBank.push(numbersYes)
+  randomArr.push(...numbersYes)
 }
 if (specchar){
-  randomBank.push(scharsYes)
+  randomArr.push(...scharsYes)
 }
-console.log(randomBank);
+console.log(randomArr);
+
+while (true){
 
 
-
-
+let acceptableCriteria = true
+let usesLower = false
+let usesCaps = false
+let usesNumbers = false
+let usesSchars= false
+let password=""
 
 for (let i = 0; i < passwordLength; i++){
-  let upperRandom = uppercaseYes[Math.floor(Math.random() * uppercaseYes.length)]
-  alert(upperRandom)
-  //let lowerRandom = lowercaseYes[Math.floor(Math.random() * lowercaseYes.length)]
- // let numberRandom = numbersYes[Math.floor(Math.random() * numbersYes.length)]
-  //let scharsRandom = scharsYes[Math.floor(Math.random * scharsYes.length)
-  
+  password += randomArr[Math.floor(Math.random() * randomArr.length)]
+ 
 }
+
+return password
+
+if (lCase){
+  for (i=0; i< lowercaseYes.length; i++){
+  usesLower = password.includes(lowercaseYes[i])
+  if (usesLower){
+    break
+    }
+  }
+}
+
+if (uCase){
+  for (i=0; i< uppercaseYes.length; i++){
+  usesCaps = password.includes(uppercaseYes[i])
+  if (usesCaps){
+    break
+    }
+  }
+}
+
+if (numeric){
+  for (i=0; i< numbersYes.length; i++){
+  usesNumbers = password.includes(numbersYes[i])
+  if (usesNumbers){
+    break
+    }
+  }
+}
+
+if (spechchar){
+  for (i=0; i< scharsYes.length; i++){
+  usesSchars = password.includes(scharsYes[i])
+  if (usesSchars){
+    break
+    }
+  }
+}
+
+if ((usesLower = false && lCase) || (usesCaps = false && uCase) || (usesNumbers = false && numeric) || (usesSchars = false && spechchar)){
+  acceptableCriteria = false
+}
+
+if (acceptableCriteria) {
+break 
+}
+
+return password;
+
+}
+}
+
 
 function writePassword() {
   let password = generatePassword();
   let passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
-
- 
-
-
-}
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
